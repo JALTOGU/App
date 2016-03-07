e8e4adcae94914b27043cad53d255ce9283b40e6
MARTHA <MARTHA@CASA.home>
2016-03-07 14:34:38 -0500

    RESIENTES MODICACIONES DE NOMBRE DEL INICIO SECCION

--- src/formularios/Login.java
+++ src/formularios/Login.java
@@ -33,7 +33,7 @@ public class Login extends javax.swing.JFrame {
         jLabel3 = new javax.swing.JLabel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
-        setTitle("INGRESO AL SISTEMA");
+        setTitle("INICIO DE SESION");
         setBackground(new java.awt.Color(255, 255, 255));
         setResizable(false);
 
